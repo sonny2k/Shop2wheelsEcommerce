9c07ca6eea269e09a421338d70ac367989090b60
TTL <thanh.187pm14010@vanlanguni.vn>
2024-02-23 15:03:52 +0700

    feat: add create new user and implemnt html page for create new user
    successfully created user will have an alert displayed to notify the admin

--- Shop2wheelsProject/Shop2wheelsWebParent/Shop2wheelsBackEnd/src/main/java/com/shop2wheels/admin/user/UserController.java
+++ Shop2wheelsProject/Shop2wheelsWebParent/Shop2wheelsBackEnd/src/main/java/com/shop2wheels/admin/user/UserController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shop2wheels.common.entity.Role;
 import com.shop2wheels.common.entity.User;
 
 @Controller
@@ -21,5 +24,27 @@ public class UserController {
 		model.addAttribute("userslist", usersList);
 
 		return "users";
+	}
+
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> rolesList = service.listRoles();
+
+		User user = new User();
+		user.setEnabled(true);
+
+		model.addAttribute("user", user);
+		model.addAttribute("rolesList", rolesList);
+
+		return "user_form";
+	}
+
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		return "redirect:/users";
 	}
 }
